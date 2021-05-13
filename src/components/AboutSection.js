dfc90daaf94d8d972ccb1813140d71f3d18d15f7
M-Rb3 <rabea9220@gmail.com>
2021-05-13 13:47:31 +0200

    our work and about us animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,54 @@
 import React from "react";
 import home1 from "../img/home1.png";
 // Styled
-import styled from "styled-components";
 import { About, Description, Image, Hide } from "../style";
+// Framer Motion
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
+
 const AboutSection = () => {
+  // const titleAnimation = {
+  //   hidden: { opacity: 0 },
+  //   show: { opacity: 1, transition: { duration: 2 } },
+  // };
+  // const container = {
+  //   hidden: { x: 100 },
+  //   show: {
+  //     x: 0,
+  //     transition: {
+  //       duration: 0.75,
+  //       ease: "easeOut",
+  //       staggerChildren: 1,
+  //     },
+  //   },
+  // };
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
-        </div>
-        <p>
+        </motion.div>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professtionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,19 @@
 import React from "react";
+// Animatons
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const ContactUS = () => {
   return (
-    <div>
+    <motion.div
+      variants={pageAnimation}
+      exit="exit"
+      initial="hidden"
+      animate="show"
+      style={{ background: "#fff" }}
+    >
       <h1>Contact US</h1>
-    </div>
+    </motion.div>
   );
 };
 export default ContactUS;
